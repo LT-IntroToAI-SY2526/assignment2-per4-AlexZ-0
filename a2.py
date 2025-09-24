e4f04ff27f0dd6092ce9e45260ec652f9d8edd3d
Alex Zhou <azhou5@cps.edu>
2025-09-24 11:40:44 -0500

    First commit

--- a2.py
+++ a2.py
@@ -17,34 +17,64 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     sind = 0  # current index we are looking at in source list
     pind = 0  # current index we are looking at in pattern list
     result: List[str] = []  # to store substitutions we will return if matched
+    
 
     # keep checking as long as we haven't hit the end of either pattern or source while
     # pind is still a valid index OR sind is still a valid index (valid index means that
     # the index is != to the length of the list)
-    while "FILL IN CONDITION HERE":
-        # your job is to fill out the body of this loop
+    while pind < len(pattern) or sind < len(source):
 
+        # your job is to fill out the body of this loop 
         # you should delete the following line
-        return ["Not done yet :)"]
-
+        #return ["Not done yet :)"]
         # 1) if we reached the end of the pattern but not source
-
+     if pind == len(pattern) and sind < len(source):
+            #print("Reached end of pattern, but not source")
+            return None
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
+     elif pattern[pind] == "%":
+          # If last
+        if pind == len(pattern) - 1:
+             combined = " ".join(source[sind:])
+             result.append(combined)
+             #print(result)
+             return result
+          #result.append(" ".join{source[spos:sind]})
 
-        # 3) if we reached the end of the source but not the pattern
+            # 3) if we reached the end of the source but not the pattern
+        else:
+            pind += 1
+            accum = " "
+            while pattern[pind] != source[sind]:
+                accum += source[sind] + " "
+                sind += 1
+
+                if sind == len(source):
+                    return None
 
         # 4) if the current thing in the pattern is an _
+     elif pattern[pind] == "_":
+            result.append(source[sind])
+            pind += 1
+            sind += 1
 
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
+     elif pattern[pind] == source[sind]:
+            pind += 1
+            sind += 1
 
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
-
+     else:
+            print(pattern[pind] + " " + source[sind])
+            print("No match found")
+            return None
+    print("success")
     return result
 
 
@@ -56,12 +86,13 @@ if __name__ == "__main__":
     assert match(["x", "_", "z"], ["x", "y", "z"]) == ["y"], "test 5 failed"
     assert match(["x", "_", "_"], ["x", "y", "z"]) == ["y", "z"], "test 6 failed"
     assert match(["%"], ["x", "y", "z"]) == ["x y z"], "test 7 failed"
+    assert match(["x","%"], ["x", "y", "z","a","b"]) == ["x y z"], "test 7(2) failed"
     assert match(["x", "%", "z"], ["x", "y", "z"]) == ["y"], "test 8 failed"
     assert match(["%", "z"], ["x", "y", "z"]) == ["x y"], "test 9 failed"
     assert match(["x", "%", "y"], ["x", "y", "z"]) == None, "test 10 failed"
     assert match(["x", "%", "y", "z"], ["x", "y", "z"]) == [""], "test 11 failed"
-    assert match(["x", "y", "z", "%"], ["x", "y", "z"]) == [""], "test 12 failed"
     assert match(["x", "%", "y"], ["x", "z", "z"]) == None, "test 13 failed"
+    assert match(["x", "y", "z", "%"], ["x", "y", "z"]) == [""], "test 12 failed"
     assert match(["_", "%"], ["x", "y", "z"]) == ["x", "y z"], "test 14 failed"
     assert match(["_", "_", "_", "%"], ["x", "y", "z"]) == [
         "x",
